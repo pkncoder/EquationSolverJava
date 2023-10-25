d07995faf17c5a9e1f4e69c3d67501907f983d8c
pkncoder <alex.h.preston@gmail.com>
2023-10-25 18:31:36 -0400

    Working on the test for cases

--- Main.java
+++ Main.java
@@ -4,5 +4,30 @@ public class Main {
     {
         // Ask for the equation
         String equation = GeneralFuncs.strInput("Input the equation you would like to solve: ");
+
+
+        // Get the length for how many times we need to loop
+        int equationLen = equation.length();
+
+        // Get the current sign (pos or negitive) for maths
+        String sign;
+
+
+        // Loop through the equation
+        for (int j = 0; j < equationLen; j++)
+        {
+            // If the curent spot is equal to x
+            if (equation.charAt(j) == 'x')
+            {
+
+            }
+
+            // Test for equal sign
+            if (equation.charAt(j) == '=')
+            {
+                
+            }
+            
+        }
     }
 }
